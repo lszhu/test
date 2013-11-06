4f553491a914f2a9eb5956364fcd4d585b1105c4
Jason <zhlisen@gmail.com>
2013-11-06 17:49:31 +0800

    Create db2b1.js

---
+++ db2b1.js
@@ -0,0 +1,95 @@
+/**
+ *
+ * User: Jason
+ * Date: 13-9-8
+ * Time: 上午12:24
+ *
+ */
+
+!function () {
+    var db = [
+        [
+            "X3QRfWt6zT8LiDg0", "tyMjXrz285gLoxM3"
+        ],
+        [
+            "3SWyrx5vqsmrh+Ne", "4XGaNEIwsW281xrA", "1HdfOzrcjl5hLvO3",
+            "bbLnW5C15VaVxk6m"
+        ],
+        [
+            "3SWyrx5vqsmrh+Ne", "4XGaNEhlVs1FkZ+l", "m08seMGfT21uAuqF",
+            "ZAfXue83jJMsrkpJ"
+        ],
+        [
+            "HO+H9Q6wMLFp8x8N", "yML+7Db5A2jPIjFx", "+R/vMyMQShG6pQlj",
+            "kOX1tJ91hxh7fyZ6", "m5DMXXp02n3spYIb", "uQSD4w=="
+        ],
+        [
+            "tdIVwD+x0xe3YqZY", "tMyBdBvDrSw7uxm6", "dsQ9wEH+GyAgJFzf",
+            "j0LwJAfdP2TBZbgS", "/LNB+Zsaqebk53yX", "eRKLHQ=="
+        ],
+        [
+            "tdIVwD+x0xe3YqZY", "tMyBdPjLYv46mFE1", "XpePe6c7Dmtmm4ol",
+            "ds67g1BuceHTMC0n", "0rWuB5vPxHTbpV9g", "6+7pXg=="
+        ],
+        [
+            "tdIVwD+x0xe3YqZY", "tMyBdPjLYv46mFE1", "XpePe6c7DmtNhjTR",
+            "m967f3tYKEi+rgCK", "Gb29AjG1bWXCzZJQ", "f5PO6Dur9EF7ylA2",
+            "JQOwbd5VKpo="
+        ],
+        [
+            "zmTaxDFC8mw324cU", "PnolZMuE1ss1Nevw", "fqWpIuyrPuaWo8Zy",
+            "zO6oHxgEz53vAH1y", "WumQ3p8LXKiJ+lcx", "iSDAXvAcAZEkuErg",
+            "LahHw9tbiVgPV8lb", "e/VXu+VTIghC+Iyx", "JCh/cdM8Ou2D9D5h",
+            "2+FMx+LfVBF8RfKR", "uAZf9599MrleVgve", "GJwXchbsoJfkItQi"
+        ],
+        [
+            "ABBwMTqX4tg3KO0u", "PDAgfcI6OkIZNk2b", "8cHAz7NdQO5GlheT",
+            "w3LROrdyuNQD0qIf", "iioEGQRckG9tnJAI", "SasDcHW+ahAbnG2u",
+            "ysrTDucXbPnFdGH3", "0+1OFuh2mxb0xQYP", "n5Q6FYR1hUzVgf6n",
+            "iqeockm67z0IVFWJ", "dA78Gr4eR0RQe2n4", "ukh4qq4HJROy9w1o",
+            "xEbtq5oNkgVNWPlM", "VzDCadcBGQl6pes7", "Vi8OY8DUAEA5Rcwf",
+            "xdTHHZRfOAYwYn7z", "AXwig/7yU1GAHwrs", "zMBui3yR4LaAw1m7",
+            "sto9V7Osv3tkFYVO", "JeHBfOet98mp79be", "1Tkqd7JTYzHb1/uO"
+        ],
+        [
+            "h5EWBznz9Jol2mqe", "EdCSsTQP7JT3ZaR9", "zCib+9zyc2PyLt7u"
+        ],
+        [
+            "iTtFX8WNn26P2F38", "tAFA2ibcbf9o1xsT", "mNdYG8rYgsH1QeoJ"
+        ],
+        [
+            "ABBwMTqX4tg3KO0u", "PDAgfcI6OkIZNk2b", "iqeockm67z0IVFWJ",
+            "dA78Gr4eR0RQe2n4", "JvZsiKIuSiiS5/5m", "xEbtq5oNkgVNWPlM",
+            "VzDCadcBGQl6pes7", "Vi8OY8DUAEA5Rcwf", "86V07QLnwVy9zPWx",
+            "OS9As48nwuzy3ijU", "zMBui3yR4LaAw1m7", "sto9V7Osv3tkFYVO",
+            "JeHBfOet98mp79be", "1Tkqd7JTYzHb1/uO"
+        ],
+        [
+            "BUJJZVEFcqccSMOw", "UFzCccw081GxEY2H", "8aR9u6tBU3Is0XdE",
+            "e0mtTz25F67cckAA", "CpdhgYNEU/8cIz2a", "ABBwMTqX4tg3KO0u",
+            "PDAgfcI6OkIZNk2b", "JeHBfOet98mp79be", "1Tkqd7JTYzHb1/uO"
+        ],
+        [
+            "zMBui3yR4LaAw1m7", "sto9V7Osv3tkFYVO", "JeHBfOet98mp79be",
+            "1Tkqd7JTYzHb1/uO"
+        ],
+        [
+            "BUJJZVEFcqccSMOw", "UFzCccw081GxEY2H", "JeHBfOet98mp79be",
+            "1Tkqd7JTYzHb1/uO"
+        ],
+        [
+            "BUJJZVEFcqccSMOw", "UFzCccw081GxEY2H", "W1gDbgD9E2xqAjW7",
+            "33AH5CIFOPoxS9Fu", "dA78Gr4eR0RQe2n4", "ukh4qq4HJROy9w1o",
+            "zMBui3yR4LaAw1m7", "sto9V7Osv3tkFYVO", "JeHBfOet98mp79be",
+            "1Tkqd7JTYzHb1/uO"
+        ],
+        [
+            "BUJJZVEFcqccSMOw", "UFzCccw081GxEY2H", "8aR9u6tBU3Is0XdE",
+            "e0mtTz25F67cckAA", "CpdhgYNEU/8cIz2a", "xEbtq5oNkgVNWPlM",
+            "VzDCadcBGQl6pes7", "Vi8OY8DUAEA5Rcwf"
+        ]
+    ];
+    var data = [];
+    data[8] = db;
+    commander.setter(data);
+}();
