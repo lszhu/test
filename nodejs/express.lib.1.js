307618eecef65947b754ab0cf6268897f29b4efa
Jason Zhu <jasonzhu.dev@gmail.com>
2014-04-06 14:09:39 +0800

    add paras in route path in file express.lib.1.js

--- nodejs/express.lib.1.js
+++ nodejs/express.lib.1.js
@@ -9,3 +9,11 @@ http.createServer(app).listen(3000, function() {
 app.get('/', function(req, res) {
     res.send('hello express.');
 });
+
+app.get('/:id?', function(req, res) {
+    if (req.params.id) {
+        res.send(req.params.id);
+    } else {
+        res.send('oh hai');
+    }
+});
