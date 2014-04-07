3434fee44be9fc5b58aa84ba49d540d211bfbf0e
Jason Zhu <jasonzhu.dev@gmail.com>
2014-04-07 13:57:28 +0800

    show route forward and form parser in file express.lib.4.js and express.lib.4.jade

--- nodejs/express.lib.4.js
+++ nodejs/express.lib.4.js
@@ -9,6 +9,9 @@ http.createServer(app).listen(3000, function() {
 app.use('view engine', 'jade');
 app.use(express.cookieParser());
 app.use(express.session({secret: 'secretKey'}));
+app.use(express.urlencoded());
+
+var user = {name: 'admin', password: 'default'};
 
 var roleFactory = function(role) {
     return function(req, res, next) {
@@ -16,16 +19,33 @@ var roleFactory = function(role) {
             next();
         } else {
             res.send('You are not authenticated.');
-            console.log(req.session.role);
+            console.log('session role: ' + req.session.role);
         }
     }
 };
 
-app.get('/', roleFactory('admin'), function(req, res) {
+app.get('/', roleFactory(user.name), function(req, res) {
     res.send('Welcome to Express.');
 });
 
 app.get('/auth', function(req, res) {
-    req.session.role = 'admin';
-    res.send('You have been authenticated.');
+    console.log('I am here in the get /auth.');
+    res.render(__dirname + '/express.lib.4.jade',
+        {pageTitle: 'authentication'});
+});
+
+app.post('/auth', function(req, res) {
+    if (req.body.user == user.name && req.body.password == user.password) {
+        req.session.role = 'admin';
+        res.send('You have been authenticated.');
+    } else {
+        res.send('Wrong username or password.');
+    }
+    console.log('req.body: ');
+    console.dir(req.body);
+});
+
+app.get('*', function(req, res) {
+    roleFactory('admin')(req, res);
+    res.redirect('/auth');
 });
