a4c8c87e988a7fb0146836bd61f7dbd5ce01369a
Jason Zhu <jasonzhu.dev@gmail.com>
2014-04-08 15:49:44 +0800

    multivalue setting in hash table in file redis.lib.2.js

--- nodejs/redis.lib.2.js
+++ nodejs/redis.lib.2.js
@@ -10,9 +10,28 @@ client.hset('user', 'username', 'johndoe');
 client.hset('user', 'firstname', 'john');
 client.hset('user', 'lastname', 'doe');
 
+client.hmset('user', 'address', 'Beijing China', 'mobile', '13912345678');
+
 client.hkeys('user', function(err, replies) {
     console.log('Result for user: ');
     console.log(replies.length + ' replies:');
+    replies.forEach(function(reply, i) {
+        console.log(i + ': ' + reply);
+    });
+    //client.end();
+});
+
+var hash = {
+    username: 'jackchan',
+    firstname: 'jack',
+    lastname: 'chan',
+    address: 'Shanghai China',
+    mobile: '13872362345'
+};
+client.hmset('newuser', hash);
+client.hscan('newuser', 0, function(err, replies) {
+    console.log('Result for newuser: ');
+    console.log(replies.length + ' replies');
     replies.forEach(function(reply, i) {
         console.log(i + ': ' + reply);
     });
