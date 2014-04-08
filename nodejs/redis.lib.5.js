f4c43d38ad7755adf87e1445a8adc8c27db905cc
Jason Zhu <jasonzhu.dev@gmail.com>
2014-04-08 16:27:32 +0800

    manipulate sorted set in redis in file redis.lib.5.js

---
+++ nodejs/redis.lib.5.js
@@ -0,0 +1,29 @@
+var redis = require('redis'),
+    client = redis.createClient();
+
+client.on('error', function(err) {
+    console.log('Error: ' + err);
+});
+
+var name = ['Neil', 'Peter', 'Brian', 'Scott', 'Brian', 'Deborah', 'John',
+    'Patrick', 'Mike', 'Courtney', 'Jennifer', 'Jessica', 'Joe', 'Bonnie',
+    'Vinny', 'Ramon', 'Becky', 'Sunny', 'Antone', 'John'];
+for (var i = 0; i < name.length; i++) {
+    client.zadd('contestants', Math.ceil(Math.random() * 12) + 18, name[i]);
+}
+
+client.zcard('contestants', function(err, length) {
+    if (err) {
+        return;
+    }
+    var n = Math.ceil(length / 3);
+    client.zrange('contestants', 0, n - 1, function(err, values) {
+        console.log('Young team; ' + values);
+    });
+    client.zrange('contestants', n, 2 * n - 1, function(err, values) {
+        console.log('Middle team: ' + values);
+    });
+    client.zrange('contestants', 2 * n, length, function(err, values) {
+        console.log('Elder team: ' + values);
+    });
+});
